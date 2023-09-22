2186e0c8cbf091ed54ef367ab8784d5f99a92eb8
Linger Xu <txu@iu.edu>
2023-09-22 12:49:45 -0700

    updadte figure 6 in module 4

--- Module_4_CRQA/PlotCRP.m
+++ Module_4_CRQA/PlotCRP.m
@@ -10,5 +10,5 @@ colormap([1 0 0; 1 1 1; 0 0 1]);
 pcolor(abs(rec-1));
 shading flat; axis square;
 title('Chromatic Cross-Recurrence Plot','FontSize',18,'FontName','Times New Roman')
-xlabel('Time Series of PP1','FontSize',16,'FontName','Times New Roman');
-ylabel('Time Series of PP2','FontSize',16,'FontName','Times New Roman');
+xlabel('Time Series of PP2','FontSize',16,'FontName','Times New Roman');
+ylabel('Time Series of PP1','FontSize',16,'FontName','Times New Roman');
